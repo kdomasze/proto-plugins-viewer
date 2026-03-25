4bf5b8d650c6296001c3c8f93fa01617663ae140
Kyle Domaszewicz <kyle@domaszewicz.me>
2026-03-24 17:26:19 -0700

    fix: also include the base path in vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	base: "/proto-plugins-viewer/",
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
